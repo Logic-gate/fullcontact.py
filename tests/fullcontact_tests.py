ba9f76f3dbf40f012ac4902257db1c05ffdf0760
Brendan Maione-Downing <bmdowning@bluestatedigital.com>
2015-03-14 22:47:15 -0400

    Some tests

---
+++ tests/fullcontact_tests.py
@@ -0,0 +1,31 @@
+from fullcontact import FullContact
+from nose.tools import *
+
+
+class TestFullContact(object):
+    def test_trivial_pass(self):
+        assert_equal(1, 1)
+
+    def test_init(self):
+        fc = FullContact('')
+        assert_equal(fc.api_key, '')
+
+    def test__prepare_batch_url(self):
+        fc = FullContact('')
+        assert_equal(
+            fc._prepare_batch_url(('person', {'email': 'test@test.com'})),
+            'https://api.fullcontact.com/v2/person.json?email=test%40test.com'
+        )
+
+    def test_invalid_api_keys(self):
+        fc = FullContact('')
+        r = fc.api_get('person', **{'email': 'test@test.com'})
+        assert_equal(r.status_code, 403)
+
+        test_batch = [
+            ('person', {'email': 'potato@yam.foo'}),
+            ('person', {'name': 'Bob Smith'})
+        ]
+
+        r = fc.api_batch(test_batch)
+        assert_equal(r.status_code, 403)
