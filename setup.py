2efae6133a2f324a025440caec91eda2fe190b88
Cathal Garvey <cathalgarvey@cathalgarvey.me>
2015-09-08 20:12:06 +0100

    Adding tags to setup.py

--- setup.py
+++ setup.py
@@ -33,6 +33,8 @@ setup(
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
